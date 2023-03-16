02226a6d8d3fb44e901be147bbb2c236c9632d83
elvvira <elvvira.castellanos@gmail.com>
2023-03-16 21:58:34 +0100

    clase

---
+++ src/js/bingo.js
@@ -0,0 +1,49 @@
+const bingoNumbers = document.getElementById("bingo-numbers");
+const youCard = document.getElementById("you-card");
+const pcCard = document.getElementById("pc-card");
+
+const numbersBingo = [];
+
+const createBingo = () => {
+  const fragment = document.createDocumentFragment();
+
+  for (let index = 1; index < 100; index++) {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("number");
+    cardElement.textContent = index;
+    fragment.append(cardElement);
+    numbersBingo.push(index);
+  }
+  bingoNumbers.append(fragment);
+};
+
+const createCard = () => {
+  const fragment = document.createDocumentFragment();
+
+  for (let index = 1; index < 26; index++) {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card__number", "number");
+
+    const randomNumber = Math.floor(Math.random() * numbersBingo.length + 1);
+    numbersBingo[randomNumber];
+    cardElement.textContent = numbersBingo[randomNumber];
+    numbersBingo.splice(randomNumber, 1);
+
+    fragment.append(cardElement);
+  }
+  youCard.append(fragment);
+  for (let index = 1; index < 26; index++) {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card__number", "number");
+
+    const randomNumber = Math.floor(Math.random() * numbersBingo.length + 1);
+    numbersBingo[randomNumber];
+    cardElement.textContent = numbersBingo[randomNumber];
+    numbersBingo.splice(randomNumber, 1);
+
+    fragment.append(cardElement);
+  }
+  pcCard.append(fragment);
+};
+
+export { createBingo, createCard };
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,23 @@
-// El styles lo importamos aquí, ya se carga después al compilar todo
-import '../scss/styles.scss';
-// import catIMage from '../assets/images/cat.jpeg'
-// const img = document.createElement('img');
-// img.src = catImage;
-// document.body.append(img);
-import { sayHello } from './demo.js';
-sayHello();
+import "../scss/styles.scss";
+
+import { createBingo, createCard } from "./bingo.js";
+
+const numbersBingo = [];
+
+const generateNumbersBingo = () => {
+  for (let index = 1; index < 100; index++) {
+    numbersBingo.push(index);
+  }
+};
+
+const drawNumbersBingo = () => {
+  const randomNumber = Math.floor(Math.random() * 99 + 1);
+  numbersBingo[randomNumber];
+  console.log(randomNumber);
+  numbersBingo.splice(randomNumber, 1);
+};
+
+generateNumbersBingo();
+drawNumbersBingo();
+createBingo();
+createCard();
